9bbed0ed6d2ca479a958236c8ceed10bc761a95e
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-02 19:36:27 +0700

    4 files modified, 4 added, 1 deleted

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="diygw-title diygw-top pink">
+					<i onclick="history.back(-1)" class="title-bar-icon diy-icon-back"></i> <span class="back-text" onclick="history.back(-1)">กลับ</span> <i onclick="location.href='index.html'" class="title-bar-icon diy-icon-home margin-left"></i>
+					<div class="title flex1 text-center textindent50">หน้าหลัก</div>
+				</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+	
+	
+</style>
